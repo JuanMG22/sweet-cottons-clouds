51a9b5c5695235b4aa8f14ce769589baaeca16d0
JuanMG22 <juan.m-gonzalez@outlook.com>
2022-07-28 19:14:41 -0300

    update

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import FsLightbox from 'fslightbox-react';
+import React, { useState } from 'react';
+import Title from '../Title';
+import img1 from '../../assets/1.png';
+import img2 from '../../assets/2.png';
+import img3 from '../../assets/3.png';
+import img4 from '../../assets/4.png';
 
 const Gallery = () => {
-  const images = [{}, {}, {}];
+  const rebornImages = [
+    { id: 0, images: { src1: img1, src2: img2 } },
+    { id: 1, images: { src1: img3, src2: img4 } }
+  ];
+
+  const [toggler, setToggler] = useState(false);
+
   return (
     <>
-      <h2>Reborn Babies</h2>
-      <h3>algo</h3>
+      <Title>Gallery</Title>
+      <h2 className="titulo flex justify-center">Reborn Babies</h2>
+
+      <div className="flex flex-column justify-center items-center w-100">
+        {rebornImages.map(img => (
+          <div className="fl w-50 flex justify-center" key={img.id}>
+            <img onClick={() => setToggler(!toggler)} src={img.images.src1} alt="" />
+            <img onClick={() => setToggler(!toggler)} src={img.images.src2} alt="" />
+          </div>
+        ))}
+      </div>
+
+      <FsLightbox toggler={toggler} type="image" sources={[img1, img2, img3, img4]} />
+
+      <style jsx>{`
+        .titulo {
+          font-size: 350%;
+          font-family: 'Italiana';
+          font-style: normal;
+          font-weight: 600;
+          line-height: 118px;
+        }
+      `}</style>
     </>
   );
 };
