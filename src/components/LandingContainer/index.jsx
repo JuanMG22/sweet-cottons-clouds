6993033cc190fe3a403dd43802cb8d3b3c1c3c16
Gonzalo Leguizamon <gonzaforce@gmail.com>
2022-07-28 12:29:47 -0300

    feat: correct some files

--- src/components/LandingContainer/index.jsx
+++ src/components/LandingContainer/index.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import carrousel from '../../assets/carrousel.png';
 
-const Home = () => {
-  return (
-    <>
-      <div className="w-100 mt3">
-        <img className="w-100 h-carrousel fit br2 shadow-5" src={carrousel} />
-      </div>
-      <h2 className="bg-divider h4 mt4 flex justify-center items-center f-headline">
-        Reborn Babies
-      </h2>
-      <style jsx>{`
-        .h-carrousel {
-          height: 400px;
-        }
-      `}</style>
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <div className="w-100 mt3">
+      <img className="w-100 h-carrousel fit br2 shadow-5" src={carrousel} />
+    </div>
+    <h2 className="bg-divider h4 mt4 flex justify-center items-center f-headline">Reborn Babies</h2>
+    <style jsx>{`
+      .h-carrousel {
+        height: 400px;
+      }
+    `}</style>
+  </>
+);
 
 export default Home;
