5476c81dd163613830fe0489238cdb1adcf8d02d
JuanMG22 <juan.m-gonzalez@outlook.com>
2022-07-31 14:52:08 -0300

    correccion-galeria

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,10 +1,12 @@
-import FsLightbox from 'fslightbox-react';
-import React, { useState } from 'react';
-import Title from '../Title';
+import React from 'react';
+import Title from '../Title/index';
 import img1 from '../../assets/1.png';
 import img2 from '../../assets/2.png';
 import img3 from '../../assets/3.png';
 import img4 from '../../assets/4.png';
+
+import '../../../node_modules/photoswipe/dist/photoswipe.css';
+import { Gallery as MyGallery, Item } from 'react-photoswipe-gallery';
 
 const Gallery = () => {
   const rebornImages = [
@@ -12,22 +14,42 @@ const Gallery = () => {
     { id: 1, images: { src1: img3, src2: img4 } }
   ];
 
-  const [toggler, setToggler] = useState(false);
-
   return (
     <>
       <Title>Gallery</Title>
       <h2 className="f1 italiana flex justify-center">Reborn Babies</h2>
 
       <div className="flex flex-column justify-center items-center w-100">
-        {rebornImages.map(img => (
-          <div className="fl w-50 flex justify-center" key={img.id}>
-            <img onClick={() => setToggler(!toggler)} src={img.images.src1} alt="" />
-            <img onClick={() => setToggler(!toggler)} src={img.images.src2} alt="" />
-          </div>
-        ))}
+        <MyGallery>
+          {rebornImages.map(img => (
+            <div className="fl w-50 flex justify-center" key={img.id}>
+              <Item
+                cropped
+                original={img.images.src1}
+                thumbnail={img.images.src1}
+                width="680"
+                height="680"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={img.images.src1} alt="reborn bebe" />
+                )}
+              </Item>
+
+              <Item
+                cropped
+                original={img.images.src2}
+                thumbnail={img.images.src2}
+                width="680"
+                height="680"
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={img.images.src2} alt="reborn bebe" />
+                )}
+              </Item>
+            </div>
+          ))}
+        </MyGallery>
       </div>
-      <FsLightbox toggler={toggler} type="image" sources={[img1, img2, img3, img4]} />
     </>
   );
 };
